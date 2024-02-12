80f3943b7fe0ad1893e3c267ce90dc7570a82ffd
vladopot <vladsopot@mail.ru>
2024-02-12 17:08:07 +0100

    feat: add server base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello');
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer((request, response) => {
+    const { method, url, headers } = request;
+
+    
+});
+
+server.listen(PORT, () => {
+    console.log(`Server has been running on the ${PORT} port`);
+});
